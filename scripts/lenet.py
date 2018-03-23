ad6572af810b6d2dc970da5a3685d63510f67238
Luke Merrick <contact@lukemerrick.com>
2018-03-23 22:07:40 +0000

    setting up old-school-style mnist experiments

--- scripts/lenet.py
+++ scripts/lenet.py
@@ -7,10 +7,10 @@ import torch.nn.functional as F
 class LeNet(nn.Module):
 
     def __init__(self):
-        super(Net, self).__init__()
+        super(LeNet, self).__init__()
         # 1 input image channel, 6 output channels, 5x5 square convolution
         # kernel
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         # an affine operation: y = Wx + b
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
@@ -27,3 +27,10 @@ class LeNet(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
--- scripts/sgd_vs_adam.py
+++ scripts/sgd_vs_adam.py
@@ -1,4 +1,5 @@
 from mlp import MLP
+from lenet import LeNet
 from wideresnet import WideResNet
 import dnn_utils
 import experiment_utils
@@ -11,38 +12,70 @@ import pytorch4adam
 
 import tensorboard_logger
 
+import itertools
 from concurrent.futures import ProcessPoolExecutor
 
-# experimental config
-epochs = 100
-logdir = '../runs/sgd_vs_adam/'
 
-def get_resnet(multi_gpu=True):
+def get_resnet(multi_gpu=False):
     model = WideResNet(depth=16, num_classes=10, widen_factor=2, drop_rate=0.0)
     if multi_gpu:
         model = torch.nn.DataParallel(model)
     return model.cuda()
 
-criterion = nn.CrossEntropyLoss().cuda()
-adam_lr = 0.0003
-sgd_lr = 0.5
-model_name = 'wideresnet_16_2'
+def get_lenet():
+    return LeNet().cuda()
 
-for trial in range(5):
-    config_model_name = '[trial_{}]'.format(trial) + model_name
-    # run ADAM
-    experiment_utils.run_experiment(model=get_resnet(multi_gpu=False), lr=adam_lr, epochs=epochs,
-            adaptive=True, amsgrad=False, model_name=config_model_name,
-            schedule_name='no_lr_decay', logdir=logdir)
+def get_single_layer_mlp():
+    return MLP([28*28, 1024, 10]).cuda()
 
-    # run SGD smooth decay
-    experiment_utils.run_experiment(model=get_resnet(multi_gpu=False), lr=sgd_lr, epochs=epochs,
-            decay_delta=0.95, decay_k=1, model_name=config_model_name,
-            schedule_name='smooth_0.95', logdir=logdir)
-    # # run SGD stepped decay
-    # experiment_utils.run_experiment(model=get_resnet(multi_gpu=False), lr=sgd_lr, epochs=epochs,
-    #         decay_delta=0.01, decay_k=40, model_name=config_model_name,
-    #         schedule_name='step_.01_40', logdir=logdir)
+def get_double_layer_mlp():
+    return MLP([28*28, 1024, 1024, 10]).cuda()
 
 
+# define experiment generator for paralellization
+def experiment_generator(trials=10, model_func=get_lenet, model_name='lenet',
+        sgd_lr=0.03, adam_lr=0.0003, task='mnist', epochs=40, logdir='../runs/'):
+    for trial in range(trials):
+        config_model_name = '[trial_{}]{}_'.format(trial, task) + model_name
+        # run ADAM
+        kwargs = dict(lr=adam_lr, epochs=epochs,
+                task='mnist',
+                adaptive=True, amsgrad=False, model_name=config_model_name,
+                schedule_name='no_lr_decay', logdir=logdir)
+        yield model_func, kwargs
 
+        # run amsgrad
+        kwargs = dict(lr=adam_lr, epochs=epochs,
+                task='mnist',
+                adaptive=True, amsgrad=True, model_name=config_model_name,
+                schedule_name='no_lr_decay', logdir=logdir)
+        yield model_func, kwargs
+    
+        # run SGD smooth decay
+        kwargs = dict(lr=sgd_lr, epochs=epochs,
+                task='mnist',
+                decay_delta=0.95, decay_k=1, model_name=config_model_name,
+                schedule_name='smooth_0.95', logdir=logdir)
+        yield model_func, kwargs
+        
+        # # run SGD stepped decay
+        # experiment_utils.run_experiment(model=get_resnet(multi_gpu=False), lr=sgd_lr, epochs=epochs,
+        #         decay_delta=0.01, decay_k=40, model_name=config_model_name,
+        #         schedule_name='step_.01_40', logdir=logdir)
+
+def run_experiment_from_generator(tpl):
+    model_func, kwargs = tpl
+    model = model_func()
+    experiment_utils.run_experiment(model, **kwargs)
+
+def run_paralell_experiment(generator, n_processes=4, trials=30):
+    criterion = nn.CrossEntropyLoss().cuda()
+    with ProcessPoolExecutor(max_workers=n_processes) as executor:
+        executor.map(run_experiment_from_generator, generator)
+
+common_config = dict(trials=30, sgd_lr=0.03, adam_lr=0.0003, task='mnist',
+                     epochs=40, logdir = '../runs/mnist_comparisons/')
+models = (('lenet',get_lenet), ('mlp_1024', get_single_layer_mlp), ('mlp_1024_1024', get_double_layer_mlp))
+generators = [experiment_generator(model_func=func, model_name=name, **common_config) for name, func in models]
+experiments = [exp for gen in generators for exp in gen]
+run_paralell_experiment(experiments)
