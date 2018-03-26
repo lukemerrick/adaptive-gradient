8f60bdf6a33c9910f1ab9b2825bffccc1b7478be
Luke Merrick <contact@lukemerrick.com>
2018-03-26 22:23:40 +0000

    refining mnist experiments to tune lr and compare lr decay strategies

--- scripts/mnist_lr_tuning.py
+++ scripts/mnist_lr_tuning.py
@@ -55,22 +55,22 @@ def experiment_batch_generator(trials=10, model_func=get_lenet, model_name='lene
         # run SGD smooth decay
         kwargs = dict(lr=sgd_lr, epochs=epochs,
                 task='mnist',
-                decay_delta=0.96, decay_k=1, model_name=config_model_name,
-                schedule_name='smooth_0.96', logdir=logdir)
+                decay_delta=0.95, decay_k=1, model_name=config_model_name,
+                schedule_name='smooth_0.95', logdir=logdir)
         batch.append((model_func, kwargs))
 
          # run SGD stepped decay
         kwargs = dict (lr=sgd_lr, epochs=epochs,
                 task='mnist',
-                decay_delta=0.45, decay_k=15, model_name=config_model_name,
-                schedule_name='step_.45_10', logdir=logdir)
+                decay_delta=0.5, decay_k=10, model_name=config_model_name,
+                schedule_name='step_.5_10', logdir=logdir)
         batch.append((model_func, kwargs))
 
         # run with different step
         kwargs = dict (lr=sgd_lr, epochs=epochs,
                 task='mnist',
-                decay_delta=0.2, decay_k=30, model_name=config_model_name,
-                schedule_name='step_.2_30', logdir=logdir)
+                decay_delta=0.2, decay_k=15, model_name=config_model_name,
+                schedule_name='step_.2_15', logdir=logdir)
         batch.append((model_func, kwargs))
         yield batch
 def run_experiment_from_generator(tpl):
@@ -86,13 +86,14 @@ def run_paralell_experiment(generator, n_processes=10):
 sgd_lrs = [0.01, 0.033, 0.1, 0.33, 1]
 adam_lrs = [0.0001, 0.00033, 0.001, 0.0033, 0.01]
 
-common_config = dict(trials=30, task='mnist',
-                     epochs=100, logdir = '../runs/mnist_sgd_vs_adam/')
-model_configs = (('lenet', get_lenet, 0.033, 0.0033),
-                  ('mlp_1024', get_single_layer_mlp, 0.1, 0.00033), 
-                  ('mlp_1024_1024', get_double_layer_mlp, 0.033, 0.0001))
-for model_name, model_func, sgd_lr, adam_lr in model_configs:
-    generator = experiment_batch_generator(model_func=model_func, model_name=model_name,
-		    sgd_lr=sgd_lr, adam_lr=adam_lr, **common_config)
-    for batch in generator:
-        run_paralell_experiment(batch, n_processes=5)
+common_config = dict(trials=1, task='mnist',
+                     epochs=10, logdir = '../runs/mnist_lr_tuning/')
+models = (('lenet',get_lenet), ('mlp_1024', get_single_layer_mlp), ('mlp_1024_1024', get_double_layer_mlp))
+for model_name, model_func in models:
+    for i in range(len(sgd_lrs)):
+        sgd_lr = sgd_lrs[i]
+        adam_lr = adam_lrs[i]
+        generator = experiment_batch_generator(model_func=model_func, model_name=model_name,
+                        sgd_lr=sgd_lr, adam_lr=adam_lr, **common_config)
+        for batch in generator:
+            run_paralell_experiment(batch, n_processes=5)
